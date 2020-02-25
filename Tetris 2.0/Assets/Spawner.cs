a51c33367ee743ad7b3fe6ada4ca5ed47fb6ee83
CloudeIdd <60155438+CloudeIdd@users.noreply.github.com>
2020-02-25 17:28:37 -0600

    Tetris
    Version 1.2

---
+++ Tetris 2.0/Assets/Spawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] Piezas;
+    void Start()
+    {
+        NewPieza();
+    }
+    public void NewPieza()
+    {
+        Instantiate(Piezas[Random.Range(0, Piezas.Length)], transform.position, Quaternion.identity);
+    }
+}
